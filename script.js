c10891312121155b846948169243d1efb80ac8bb
Esa Mohammad <113727503+esamohammad@users.noreply.github.com>
2023-06-05 10:58:34 +0600

    Eidul Ajha

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ const loading = document.getElementById('loading');
 
 const currentYear = new Date().getFullYear();
 
-const newYearTime = new Date(`April 23 ${currentYear} 00:00:00`);
+const newYearTime = new Date(`June 30 ${currentYear} 00:00:00`);
 
 // Set background year
 year.innerText = currentYear ;
